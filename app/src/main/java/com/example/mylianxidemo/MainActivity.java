c412614dcc8b3ebc815c3008a7e8d6a8c3e33b9f
libaibing123 <309183559@qq.com>
2018-06-04 18:32:11 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mylianxidemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mylianxidemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //你说你是傻逼吧 还不信  
+    }
+}
